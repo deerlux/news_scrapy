f3eb7c8faa0b287c9acec516838367cfd28b2d15
lxq <deerlux@gmail.com>
2014-09-18 17:33:02 +0800

    添加判断是否从页面中成功解析出数据

--- sinanews_scrapy/spiders/sinanews_spider.py
+++ sinanews_scrapy/spiders/sinanews_spider.py
@@ -10,7 +10,7 @@ from sinanews_scrapy.items import SinanewsScrapyItem
 class SinanewsSpider(CrawlSpider):
     name = 'sinanews'
     allowed_domains=['sina.com.cn']
-    start_urls = ['http://search.sina.com.cn/?q=%CF%B0%BD%FC%C6%BD+%BD%B2%BB%B0&range=title&c=news&sort=time']
+    start_urls = ['http://search.sina.com.cn/?q=%D2%C1%CB%B9%C0%BC%B9%FA&range=title&c=news&sort=time']
 
     '''follow=True 很关键啊，也很漂亮了，只想自己再写一个回调函数如何跟踪这个
 链接呢，原来有更方便的实现方法，害我白熬了两个小时的夜，github上找实例很重要
@@ -26,16 +26,17 @@ class SinanewsSpider(CrawlSpider):
         news = SinanewsScrapyItem()
         temp_dict = {}
 
-        news['title'] = \
-                response.xpath("//h1[@id='artibodyTitle']//text()").extract()[0]
+        temp = response.xpath("//h1[@id='artibodyTitle']//text()").extract()
+        news['title'] = temp[0] if temp else ''
 
-        news['source'] = \
-                response.xpath("//span[@id='media_name']//text()").extract()[0]
-
-        news['public_time'] = \
-                response.xpath("//span[@id='pub_date']//text()").extract()[0]
+        temp = response.xpath("//span[@id='media_name']//text()").extract()
+        news['source'] = temp[0] if temp else ''
+        
+        
+        temp =  response.xpath("//span[@id='pub_date']//text()").extract()
+        news['public_time'] = temp[0] if temp else ''
 
         temp = response.xpath("//div[@id='artibody']//p//text()").extract()
-        news['text'] = '\n'.join(temp)
+        news['text'] = '\n'.join(temp) if temp else ''
 
         return news
