7818c36d4c5cd7b731735990d9ae6b382daf5093
deerlux <deerlux@gmail.com>
2014-09-28 22:57:54 +0800

    实现了向postgresql 数据库中存储数据的一个类映射 修改了新闻数据库结构。

---
+++ sinanews_scrapy/NewsDataDb.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class NewsDataDB:
+    
+    def __init__(self, user, password, 
+            hostname='localhost', dbname='news_data',dbms = 'postgres'):
+        
+        if dbms.lower() == 'postgres':
+            engine_str = 'postgres://{0}:{1}@{2}/{3}'.format(user,
+                    password, hostname, dbname)
+        else:            
+            raise AssertionError('Wrong dbms type: %s' % dbms)
+        
+        self.engine = sqlalchemy.create_engine(engine_str)
+
+        Session = sessionmaker(bind = self.engine)
+        self.session = Session()
+
+        metadata = sqlalchemy.MetaData(bind = self.engine)
+        Base = declarative_base(metadata)
+
+        class NewsData(Base):
+            __table__ = sqlalchemy.Table('news_data', metadata, 
+                    autoload = True)
+        
+        self.NewsData = NewsData
+        self.query = self.session.query
+        self.add = self.session.add
+        self.commit = self.session.commit
+        self.rollback = self.session.rollback
+        self.flush = self.session.flush
+
+    def __del__(self):
+        self.session.close()
+
+
--- sinanews_scrapy/postgresql.sql
+++ sinanews_scrapy/postgresql.sql
@@ -5,32 +5,36 @@ create table if not exists news_data (
     public_time timestamp,
     body text,
     crawl_time timestamp,
-    url varchar(160)
-);
-
-create table if not exists keywords (
-    news_id int primary key references news_data(id),
-    keywords varchar(40)
-);
-
-create table if not exists news_domain (
-    news_id int primary key references news_data(id),
+    url varchar(160),
+    keywords varchar(40),
     domain varchar(60)
 );
 
-create view news_view as
-    select news_data.id, title, 
-    source, public_time, crawl_time, 
-    url, keywords, domain, news_data.body 
-    from news_data, keywords, news_domain
-    where news_data.id = keywords.news_id 
-    and news_data.id = news_domain.news_id;
+--
+
+--create table if not exists keywords (
+--    news_id int primary key references news_data(id),
+--    keywords varchar(40)
+--);
+
+--create table if not exists news_domain (
+--    news_id int primary key references news_data(id),
+--    domain varchar(60)
+--);
+
+--create view news_view as
+--    select news_data.id, title, 
+--    source, public_time, crawl_time, 
+--    url, keywords, domain, news_data.body 
+--    from news_data, keywords, news_domain
+--    where news_data.id = keywords.news_id 
+--    and news_data.id = news_domain.news_id;
 
 create index news_data_title_idx on news_data(title);
 create index news_data_pulic_time_idx on news_data(public_time);
 create index news_data_crawl_time_idx on news_data(crawl_time);
-create index keywords_keyword_idx on keywords(keywords);
-create index news_domain_domain_idx on news_domain(domain);
+create index news_data_keywords_idx on news_data(keywords);
+create index news_data_domain_idx on news_data(domain);
 
 
 
